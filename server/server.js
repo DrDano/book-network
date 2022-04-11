f21564cab2f3af354d6d34721a2baa73b19d9d07
DrDano <danielharned@gmail.com>
2022-04-10 20:13:16 -0700

    adding production environment conditional statement

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
